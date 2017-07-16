f511f82c5f89240e27bf52192610d8f57e72175c
Craig Brunner <craigcbrunner@gmail.com>
2017-07-16 02:41:33 -0700

    Holy moly took forever to get npm to work correctly on ubuntu subsystem for windows.. project working now, and added bootstrap for styling.

---
+++ src/components/SugarHigh.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sugarhigh">
+    <h1> Sugar high main app.. </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sugarhigh',
+  data () {
+    return {
+      msg: 'Welcome to SugarHigh'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
